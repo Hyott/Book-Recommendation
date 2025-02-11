3f7eaf0f8ca94a622d99f03d1fca25bfabc50d7d
jin0choi1216 <jin0choi1216@gmail.com>
2025-02-11 12:00:17 +0900

    refactor: 중복 코드 제거

--- app/database/crud.py
+++ app/database/crud.py
@@ -6,10 +6,6 @@ from zoneinfo import ZoneInfo
 import uuid
 from sqlalchemy.dialects.postgresql import insert
 
-# # ✅ 전체 도서 목록 조회
-# def get_all_books(db: Session):
-#     return db.query(BookTable).all()
-
 # ✅ 특정 ISBN으로 도서 조회
 def get_book_by_isbn(db: Session, isbn: str):
     return db.query(BookTable).filter(BookTable.isbn == isbn).first()
@@ -22,7 +18,6 @@ def get_sentence_by_isbn(db: Session, isbn: str):
 def get_tags_by_isbn(db: Session, isbn: str):
     return db.query(TagTable.tag_name).filter(TagTable.isbn == isbn).all()
 
-
 # ✅ 유저의 log 생성
 def add_user_response(response: UserResponseSchema):
     user_id = response.user_id if response.user_id else str(uuid.uuid4())
@@ -34,14 +29,6 @@ def add_user_response(response: UserResponseSchema):
             datetime=datetime.now(ZoneInfo("Asia/Seoul"))
         ).on_conflict_do_nothing()
     return stmt
-
-# def get_question_number_by_user_id(db: Session, user_id: str):
-#     return (
-#         db.query(UserResponseTable.user_id)
-#         .filter(UserResponseTable.user_id == user_id)
-#         .order_by(UserResponseTable.id.desc())  # 최신순 정렬
-#         .first()  # 첫 번째(가장 최신) 값 가져오기
-#     )
 
 def get_question_number_by_user_id(db: Session, user_id: str):
     return db.query(UserResponseTable.question_number) \
