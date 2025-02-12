eb1f50d537e72e61e2c94cf28564ff44ed493354
jin0choi1216 <jin0choi1216@gmail.com>
2025-02-12 15:19:55 +0900

    refactor: baseUrl 환경변수로 가져온다.

--- flutter_app/lib/question_page.dart
+++ flutter_app/lib/question_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:uuid/uuid.dart';
+import 'dart:io';
 
 class RecommendationScreen extends StatefulWidget {
   @override
@@ -9,7 +10,7 @@ class RecommendationScreen extends StatefulWidget {
 }
 
 class _RecommendationScreenState extends State<RecommendationScreen> {
-  final String baseUrl = "http://127.0.0.1:8000"; // FastAPI 백엔드 주소
+  final String baseUrl = Platform.environment['API_BASE_URL']!;
   final String userId = const Uuid().v4(); // UUID 생성
   // int questionNumber = 0; // 현재 질문 번호
 
