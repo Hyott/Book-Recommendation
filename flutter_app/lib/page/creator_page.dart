699e6a5b837601d811f34086d16976de306848de
yeeun <yeeun3578@naver.com>
2025-02-18 16:59:45 +0900

    feat: 최종 수정

--- flutter_app/lib/page/creator_page.dart
+++ flutter_app/lib/page/creator_page.dart
@@ -46,10 +46,14 @@ class CreatorScreen extends StatelessWidget {
             Align(
               alignment: Alignment.topCenter,
               child: Text(
-                '어떤 책을 읽어야 할지 망설여질 때.\n\n'
-                    '책을 고르는 순간이 설렘으로 가득 차길 바랍니다.\n\n'
-                    '새로운 취향의 책을 발견하고 싶을 때,\n\n'
-                    '책을 통해 더 넓은 세상을 만나는 여정을 함께하겠습니다.\n\n',
+                '책을 고르는 순간이 설렘으로 가득 차길 바랍니다.\n\n\n\n'
+                    '서점에서 어떤 책을 읽어야 할지 망설여질 때,\n\n'
+                    '혹은 새로운 취향의 책을 발견하고 싶을 때,\n\n'
+                    'fromSentence가 당신만을 위한 특별한 한 권을 찾아드릴게요.\n\n'
+                    '감성과 효율을 동시에 담아, 더 깊이 있는 독서 경험을 선물하는 것.\n\n'
+                    '그것이 fromSentence를 만든 toReader 팀의 바람입니다.\n\n'
+                    '책을 통해 더 넓은 세상을 만나는 여정,\n\n'
+                    '이제 fromSentence와 함께하세요.\n\n',
                 style: TextStyle(
                   fontFamily: 'Spectral-Regular',
                   fontSize: 13,
